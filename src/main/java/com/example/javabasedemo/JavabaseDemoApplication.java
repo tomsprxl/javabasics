4a876463bfab22a389f618b5ded863b9b986ca82
xuliang <xuliang@efunong.com>
2019-08-30 18:20:40 +0800

    update code

---
+++ src/main/java/com/example/javabasedemo/JavabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavabaseDemoApplication.class, args);
+    }
+
+}

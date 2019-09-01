7aab5d65c1958532c90751f3b496117752cc2244
xuliang <18656756681@163.com>
2019-09-01 10:06:54 +0800

    update code

--- src/main/java/com/example/javabasedemo/day03/ForkJoinExample.java
+++ src/main/java/com/example/javabasedemo/day03/ForkJoinExample.java
@@ -1,5 +1,8 @@
 package com.example.javabasedemo.day03;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
 
 public class ForkJoinExample extends RecursiveTask<Integer> {
@@ -32,4 +35,12 @@ public class ForkJoinExample extends RecursiveTask<Integer> {
         }
         return result;
     }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ForkJoinExample example = new ForkJoinExample(1, 10000);
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        Future result = forkJoinPool.submit(example);
+        System.out.println(result.get());
+    }
+
 }
